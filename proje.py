1ac2e43260dbb20a7bff1c9554b93430529ccaf2
Idal Kizilkurt <idalkizilkurt03@gmail.com>
2026-08-05 15:06:47 +0300

    Add student name to output

--- proje.py
+++ proje.py
@@ -1,10 +1,13 @@
 from grade_utils import calculate_average, determine_letter_grade
 
+student_name = input("Enter student name: ")
 midterm = float(input("Enter midterm grade: "))
 final = float(input("Enter final grade: "))
 
 average = calculate_average(midterm, final)
 letter = determine_letter_grade(average)
 
-print(f"\nAverage: {average:.2f}")
+print("\n----- RESULT -----")
+print(f"Student: {student_name}")
+print(f"Average: {average:.2f}")
 print(f"Letter Grade: {letter}")
